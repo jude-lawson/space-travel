77fc19a2d162aa127ac794f65e38719b96f2b7e0
Jude Lawson <lawsonjdutton@gmail.com>
2018-06-16 21:37:16 -0600

    Implements show endpoint for spaceport

--- app/controllers/api/v1/spaceports_controller.rb
+++ app/controllers/api/v1/spaceports_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::SpaceportsController < ApplicationController
   def index
     render json: Spaceport.all
   end
+
+  def show
+    render json: Spaceport.find(params[:id])
+  end
 end
--- spec/api/v1/spaceports_requests_spec.rb
+++ spec/api/v1/spaceports_requests_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Spaceports Endpoints' do
   context 'All Spaceports' do
-    it 'should show all spaceports' do
+    it 'should return all spaceports' do
       spaceports = create_list(:spaceport, 4)
 
       get api_v1_spaceports_path
@@ -10,6 +10,19 @@ RSpec.describe 'Spaceports Endpoints' do
 
       expect(response).to be_successful
       expect(response_data).to eq(spaceports.as_json)
+    end
+  end
+
+  context 'A single spaceport' do
+    it 'should return the information about a specific station' do
+      spaceport = create(:spaceport)
+
+      get api_v1_spaceport_path(spaceport)
+
+      response_data = JSON.parse(response.body)
+
+      expect(response).to be_successful
+      expect(response_data).to eq(spaceport.as_json)
     end
   end
 end
